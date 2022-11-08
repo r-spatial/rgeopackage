e89d47f57bb52aeec25468bce1275a7f07c004aa
Eli Pousson <eli.pousson@gmail.com>
2022-11-07 22:59:08 -0500

    refactor: add read_gpkg_table function

---
+++ R/read_gpkg.R
@@ -0,0 +1,72 @@
+#' Read a GeoPackage table
+#'
+#' @inheritParams connect_gpkg
+#' @param table_name Name of a GeoPackage table to read
+#' @noRd
+read_gpkg_table <- function(dsn = NULL,
+                            con = NULL,
+                            table_name = NULL,
+                            call = .envir,
+                            .envir = parent.frame()) {
+  con <- connect_gpkg(dsn, con, call, .envir)
+
+  if (is.null(table_name)) {
+    cli_abort(
+      "{.arg table_name} must be provided.",
+      call = call,
+      .envir = .envir
+    )
+  }
+
+  cli_inform("Accessing the {.val {table_name}} table.")
+  table <- DBI::dbReadTable(con, table_name)
+  RSQLite::dbDisconnect(con)
+  table
+}
+
+
+#' Does the named table exist for this connection?
+#'
+#' @noRd
+check_table_exists <- function(con,
+                               table_name = NULL,
+                               call = .envir,
+                               .envir = parent.frame()) {
+    if (!is.null(table_name) && RSQLite::dbExistsTable(con, table_name)) {
+        return(invisible(NULL))
+    }
+
+    cli_abort(
+        "Table {.val {table_name}} can't be found for the provided
+        {.arg dsn} or {.arg con}.",
+        call = call,
+        .envir = .envir
+    )
+}
+
+#' Update a selected table using a provided query string
+#'
+#' @noRd
+#' @importFrom RSQLite dbExecute
+update_gpkg_table <- function(con,
+                              table_name = NULL,
+                              statement = NULL,
+                              message = NULL,
+                              quiet = FALSE,
+                              .envir = parent.frame()) {
+    statement <- glue_sql("UPDATE {`table_name`} {statement}", .con = con)
+    n_rows <- RSQLite::dbExecute(con, statement)
+    if (!quiet) {
+        if (is.null(message)) {
+            cli_inform(
+                "Updated {n_rows} row{?s} in the {.val {table_name}} table."
+            )
+        } else {
+            cli_inform(
+                message,
+                .envir = .envir
+            )
+        }
+    }
+}
+
--- R/utils.R
+++ R/utils.R
@@ -60,51 +60,6 @@ connect_gpkg <- function(dsn = NULL,
   con %||% RSQLite::dbConnect(RSQLite::SQLite(), dsn)
 }
 
-#' Does the named table exist for this connection?
-#'
-#' @noRd
-check_table_exists <- function(con,
-                               table_name = NULL,
-                               call = .envir,
-                               .envir = parent.frame()) {
-  if (!is.null(table_name) && RSQLite::dbExistsTable(con, table_name)) {
-    return(invisible(NULL))
-  }
-
-  cli_abort(
-    "Table {.val {table_name}} can't be found for the provided
-        {.arg dsn} or {.arg con}.",
-    call = call,
-    .envir = .envir
-  )
-}
-
-#' Update a selected table using a provided query string
-#'
-#' @noRd
-#' @importFrom RSQLite dbExecute
-update_gpkg_table <- function(con,
-                              table_name = NULL,
-                              statement = NULL,
-                              message = NULL,
-                              quiet = FALSE,
-                              .envir = parent.frame()) {
-  statement <- glue_sql("UPDATE {`table_name`} {statement}", .con = con)
-  n_rows <- RSQLite::dbExecute(con, statement)
-  if (!quiet) {
-    if (is.null(message)) {
-      cli_inform(
-        "Updated {n_rows} row{?s} in the {.val {table_name}} table."
-      )
-    } else {
-      cli_inform(
-        message,
-        .envir = .envir
-      )
-    }
-  }
-}
-
 #' Does the query return the same or greater than the minimum number of rows?
 #'
 #' @noRd
