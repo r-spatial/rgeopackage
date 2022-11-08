9e5d94ae8200cb578733b8f800b9b4b17c36ba2a
Eli Pousson <eli.pousson@gmail.com>
2022-11-08 10:55:27 -0500

    feat(read_gpkg): add quiet parameter
    - feat(read_gpkg): add quiet parameter
    - fix(read_gpkg): replace reference to DBI w/ RSQLite

--- R/read_gpkg.R
+++ R/read_gpkg.R
@@ -2,12 +2,15 @@
 #'
 #' @inheritParams connect_gpkg
 #' @param table_name Name of a GeoPackage table to read
+#' @param quiet If `TRUE`, do not show informational messages. Defaults to
+#'   `FALSE`.
 #' @export
 read_gpkg_table <- function(dsn = NULL,
                             con = NULL,
                             table_name = NULL,
                             call = .envir,
-                            .envir = parent.frame()) {
+                            .envir = parent.frame(),
+                            quiet = FALSE) {
   con <- connect_gpkg(dsn, con, call, .envir)
 
   if (is.null(table_name)) {
@@ -18,8 +21,10 @@ read_gpkg_table <- function(dsn = NULL,
     )
   }
 
-  cli_inform("Accessing the {.val {table_name}} table.")
-  table <- DBI::dbReadTable(con, table_name)
+  if (!quiet) {
+    cli_inform("Accessing the {.val {table_name}} table.")
+  }
+  table <- RSQLite::dbReadTable(con, table_name)
   RSQLite::dbDisconnect(con)
   table
 }
