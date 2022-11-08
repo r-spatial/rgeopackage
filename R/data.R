3b36838c068c17608bf7638c7f1e409de7d0cdad
Eli Pousson <eli.pousson@gmail.com>
2022-11-08 10:54:38 -0500

    docs: add docs for extension data

---
+++ R/data.R
@@ -0,0 +1,38 @@
+#' Registered GeoPackage Extensions
+#'
+#' Reference table of registered GeoPackage extensions from
+#' <http://www.geopackage.org/guidance/getting-started.html#registered-extensions>.
+#' Note that the information in the reference column is an incomplete
+#' representation of the requirements for extension implementation. This data is
+#' provided to support a (yet to be developed) functions that allow the addition
+#' of extensions to exported GeoPackage files.
+#'
+#' @format A data frame with 9 rows and 4 variables:
+#' \describe{
+#'   \item{\code{pos}}{Position/order}
+#'   \item{\code{extension}}{Extension title}
+#'   \item{\code{url}}{Detail URL}
+#'   \item{\code{reference}}{List column with reference tables for
+#'   gpkg_extensions and other extension-specific tables.}
+#'}
+"gpkg_extensions"
+
+#' Community GeoPackage Extensions
+#'
+#' Reference table of community GeoPackage extensions from
+#' <http://www.geopackage.org/extensions.html>. Note that the information in the
+#' reference column is an incomplete representation of the requirements for
+#' extension implementation. This data is provided to support a (yet to be
+#' developed) functions that allow the addition of extensions to exported
+#' GeoPackage files.
+#'
+#' @format A data frame with 15 rows and 5 variables:
+#' \describe{
+#'   \item{\code{pos}}{Position/order}
+#'   \item{\code{extension}}{Extension title}
+#'   \item{\code{description}}{Extension description}
+#'   \item{\code{url}}{Detail URL}
+#'   \item{\code{reference}}{List column with reference tables for
+#'   gpkg_extensions and other extension-specific tables.}
+#'}
+"community_gpkg_extensions"
