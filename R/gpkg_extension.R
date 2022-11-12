10d3e0fbbf0ddf924518df0f68b88fcdedd897f1
Eli Pousson <eli.pousson@gmail.com>
2022-11-12 00:00:08 -0500

    refactor(read_gpkg_extension): incorporate reference data
    - refactor(read_gpkg_extension): incorporate reference data for extensions as a lookup table for table_name values
    - feat(read_gpkg_extension): add read_gpkg_related_tables variant
    - feat: add draft use_gpkg_extension
    - refactor: add helper extension_tables and extension_table_names functions

--- R/gpkg_extension.R
+++ R/gpkg_extension.R
@@ -1,4 +1,3 @@
-
 #' Read GeoPackage tables associated with an extension
 #'
 #' Supports the metadata extension
@@ -7,10 +6,13 @@
 #'
 #' @param extension Extension name. Required.
 #' @param table_name One or more table names required for the corresponding
-#'   extension. Required.
+#'   extension. If `NULL`, there is no check to
 #' @inheritParams read_gpkg_table
 #' @param ... Additional parameters passed to [read_gpkg_extension()] by
-#'   [read_gpkg_metadata()] or [read_gpkg_schema()].
+#'   [read_gpkg_metadata()], [read_gpkg_schema()], or
+#'   [read_gpkg_related_tables()].
+#' @return A list of dataframes including any available table associated with
+#'   the named extension.
 #' @export
 #' @importFrom RSQLite dbDisconnect
 read_gpkg_extension <- function(dsn = NULL,
@@ -23,7 +25,8 @@ read_gpkg_extension <- function(dsn = NULL,
 
   check_gpkg_extension(
     dsn = NULL, con = con,
-    extension, table_name, call, .envir
+    extension, table_name,
+    TRUE, call, .envir
   )
 
   extension_tables <-
@@ -38,17 +41,23 @@ read_gpkg_extension <- function(dsn = NULL,
     )
 
   RSQLite::dbDisconnect(con)
-  extension_tables
+
+  if (length(extension_tables) == length(table_name)) {
+    names(extensions_tables) <- table_name
+  }
+
+  extensions_tables
 }
 
 #' @name read_gpkg_schema
 #' @rdname read_gpkg_extension
 #' @export
-read_gpkg_schema <- function(dsn, ...) {
+read_gpkg_metadata <- function(dsn, ...) {
+  extension <- "gpkg_metadata"
   read_gpkg_extension(
     dsn,
-    extension = "gpkg_schema",
-    table_name = c("gpkg_data_columns", "gpkg_data_column_constraints"),
+    extension = extension,
+    table_name = extension_table_names(extension),
     ...
   )
 }
@@ -56,11 +65,25 @@ read_gpkg_schema <- function(dsn, ...) {
 #' @name read_gpkg_schema
 #' @rdname read_gpkg_extension
 #' @export
-read_gpkg_metadata <- function(dsn, ...) {
+read_gpkg_schema <- function(dsn, ...) {
+  extension <- "gpkg_schema"
   read_gpkg_extension(
     dsn,
-    extension = "gpkg_metadata",
-    table_name = c("gpkg_metadata", "gpkg_metadata_reference"),
+    extension = extension,
+    table_name = extension_table_names(extension),
+    ...
+  )
+}
+
+#' @name read_gpkg_related_tables
+#' @rdname read_gpkg_extension
+#' @export
+read_gpkg_related_tables <- function(dsn, ...) {
+  extension <- "gpkg_related_tables"
+  read_gpkg_extension(
+    dsn,
+    extension = extension,
+    table_name = extension_table_names(extension),
     ...
   )
 }
@@ -70,26 +93,47 @@ read_gpkg_metadata <- function(dsn, ...) {
 #'
 #' @param extension Extension name. Required.
 #' @param table_name One or more table names required for the corresponding
-#'   extension. Required.
+#'   extension. Required if check_table is `TRUE`.
+#' @param check_table If `TRUE` (default), error if table_name is `NULL`. If
+#'   `FALSE`, do not check for existence of table regardless of table_name
+#'   value.
 #' @noRd
 #' @importFrom RSQLite dbReadTable dbExistsTable dbDisconnect
 check_gpkg_extension <- function(dsn = NULL,
                                  con = NULL,
                                  extension,
                                  table_name,
+                                 check_table = TRUE,
                                  call = .envir,
                                  .envir = parent.frame()) {
   con <- connect_gpkg(dsn, con)
 
-  gpkg_extensions <- RSQLite::dbReadTable(con, "gpkg_extensions")
+  con_gpkg_extensions <- RSQLite::dbReadTable(con, "gpkg_extensions")
+
+  if (missing(extension) | missing(table_name)) {
+    cli_abort(
+      "Both {.arg extension} and {.arg table_name} must be provided.",
+      call = call, .envir = .envir
+    )
+  }
 
   extension_tables <-
-    gpkg_extensions[gpkg_extensions$extension_name %in% extension, ]
+    con_gpkg_extensions[con_gpkg_extensions$extension_name %in% extension, ]
 
   if (nrow(extension_tables) == 0) {
     cli_abort(
       "{.arg extension} {.val {extension}} can't be found
       in the {.val gpkg_extensions} table.",
+      call = call,
+      .envir = .envir
+    )
+  }
+
+  if (!check_table) {
+    return(invisible(NULL))
+  } else if (is.null(table_name)) {
+    cli_abort(
+      "{.arg table_name} must be provided.",
       call = call,
       .envir = .envir
     )
@@ -112,8 +156,123 @@ check_gpkg_extension <- function(dsn = NULL,
   }
 
   cli_abort(
-    "{.arg table_name} {.val {table_name[!has_tables]}} can't be found for extension {.val {extension}}.",
-    call = call,
-    .envir = .envir
+    "{.arg table_name} {.val {table_name[!has_tables]}}
+    can't be found for extension {.val {extension}}.",
+    call = call
   )
+}
+
+
+#' Get extension table names from the reference table of registered or community
+#' GeoPackage extensions
+#'
+#' @keywords internal
+extension_table_names <- function(extension,
+                                  community = FALSE,
+                                  exclude = "gpkg_extensions") {
+  table_ref <- extension_tables(extension, community)
+  table_names <- names(table_ref)
+
+  if (is.null(exclude)) {
+    return(table_names)
+  }
+
+  table_names[!(table_names %in% exclude)]
+}
+
+#' Get reference tables from gpkg_extensions or community_gpkg_extensions
+#'
+#' @keywords internal
+extension_tables <- function(extension,
+                             community = FALSE) {
+  if (community) {
+    community_gpkg_extensions$reference[[extension]]
+  } else {
+    gpkg_extensions$reference[[extension]]
+  }
+}
+
+#' Use a GeoPackage extension for a file
+#'
+#' Add an entry to the "gpkg_extensions" table for a file.
+#'
+#' @inheritParams connect_gpkg
+#' @param extension Extension name to use. Required.
+#' @param table_name One or more table names to add to "gpkg_extensions" table.
+#' @param column_name Column name.
+#' @param definition Table definition. Required.
+#' @param scope Table scope. Required. If length 1, scope is recycled for all
+#'   tables.
+#' @noRd
+use_gpkg_extension <- function(dsn = NULL,
+                               con = NULL,
+                               extension,
+                               table_name = NULL,
+                               column_name = NULL,
+                               definition = NULL,
+                               scope = NULL,
+                               reference = TRUE) {
+  con <- connect_gpkg(dsn, con)
+
+  if (reference) {
+    if (!is.null(table_name)) {
+      cli_abort(
+        "{.arg table_name} must be {.code NULL}
+          if {.arg reference} is {.code TRUE}."
+      )
+    }
+
+    extension_ref <- extension_tables(extension)[["gpkg_extensions"]]
+    table_name <- extension_ref$table_name
+
+    if (any(vapply(c(column_name, definition, scope), is.null, TRUE))) {
+      overwrite_arg <- c("column_name", "definition", "scope")
+      cli_warn(
+        "Replacing provided values for {.arg {overwrite_arg}}
+        with reference values."
+      )
+    }
+
+    column_name <- extension_ref$column_name
+    definition <- extension_ref$definition
+    scope <- extension_ref$scope
+  }
+
+  num_tables <- length(table_name)
+
+  if ((length(scope) == 1) && num_tables > 1) {
+    scope <- rep(scope, num_tables)
+  }
+
+  multiple <- FALSE
+  if (length(scope) > 1) {
+    multiple <- TRUE
+  }
+
+  scope_opts <- c("read-write", "read-only", "write-only")
+  scope <- match.arg(scope, scope_opts, multiple)
+
+  if (!is.null(definition) && (num_tables > 0) && (length(definition) != num_tables)) {
+    cli_abort(
+      "{.arg definition} length must match {.arg table_name} length."
+    )
+  }
+
+  gpkg_extensions_rows <-
+    list(
+      "table_name" = table_name,
+      "column_name" = column_name,
+      "definition" = definition,
+      "extension_name" = rep(extension, num_tables),
+      "scope" = scope
+    )
+
+  append_gpkg_table(
+    dsn = NULL,
+    con = con,
+    table_name = "gpkg_extensions",
+    table_data = as.data.frame(gpkg_extensions_rows)
+  )
+
+  RSQLite::dbDisconnect(con)
 }
